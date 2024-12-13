173b3a847c7ed4526ee7381e682d49a7cd169d69
sidram403 <siddharthpatil9108@gmail.com>
2024-12-13 22:58:33 +0530

    added employers page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      colors: {
-        verifiedGreen: '#00C853', // Replace with exact green color if needed
-        buttonBlue: '#007BFF',
-        tabGray: '#E8E8E8',
-      },
+     
       boxShadow: {
         custom: '0px 4px 4px 0px #000000', // Your custom shadow
       },
